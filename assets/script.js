0adec958c2a69fb3bcc3bbac31764d7fba47edd5
Sâmily <goessamily@gmail.com>
2025-09-01 17:07:39 -0300

    feat: Efeito de clique dos botões/links da nav

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+const links = document.querySelectorAll("nav a");
+
+
 function idade()
 {
     const txtIdade = document.querySelector("#idade");
@@ -7,4 +10,43 @@ function idade()
 
     txtIdade.innerHTML = idade;
 }
+
+
+// Verifica qual botão foi clicado, seta o id para estilização e verifica quais não foram clicados no momento
+// para retirar os ids de estilização
+function btnClicado()
+{
+    for(let i = 0; i < links.length; i++)
+    {
+        links[i].addEventListener("click", function()
+        {
+            localStorage.setItem("linkClicado", links[i]);
+            links[i].id = "pagina-atual";    
+            
+            for(let j = 0; j < links.length; j++)
+            {
+                if(links[j] != localStorage.getItem("linkClicado")) { links[j].id = ""; } 
+            }
+        });
+    }
+}
+
+// Faz com que o último botão clicado permaneça com o id para ser estilizado
+function ultimoBtnClicado()
+{
+    for(let i = 0; i < links.length; i++)
+    {
+        if(links[i] == localStorage.getItem("linkClicado"))
+        {
+            // Setando o id para estilização do botão apertado
+            links[i].id = "pagina-atual";
+        }     
+    }    
+}
+
+
+
+
 idade();
+btnClicado();
+ultimoBtnClicado();
